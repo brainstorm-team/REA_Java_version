f01708d2c62de4062fed13edd3b8fe850efd9adb
jemacom <jemacomchokri@gmail.com>
2014-11-09 14:35:33 +0100

    Fixing double page d'accueil bug

--- src/gui/Authentification.java
+++ src/gui/Authentification.java
@@ -41,12 +41,18 @@ public class Authentification extends javax.swing.JFrame {
         KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher(){
             @Override
             public boolean dispatchKeyEvent(KeyEvent e) {
-                int key = e.getKeyCode();
-                if (key == KeyEvent.VK_ENTER){
-                    seConnecter();
+                
+                    switch (e.getID()) {
+                    case KeyEvent.KEY_PRESSED:
+                        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                            seConnecter();
+                        }
+                        break;
+                        default :
+                    return false;
                 }
                 return false;
-            }
+            }     
     
         });
         
@@ -69,6 +75,7 @@ public class Authentification extends javax.swing.JFrame {
       }else if( agent1 != null){
           if(agent1.getPass().equals(pass.getText())){
               Acceuil_client ac = new Acceuil_client();
+               Util.id_agent_connecte = agent1.getId();
               ac.setVisible(true);
               this.setVisible(false);
           }
@@ -76,6 +83,7 @@ public class Authentification extends javax.swing.JFrame {
       else if (agent2 != null){
           if (agent2.getPass().equals(pass.getText())){
               Acceuil_gerant ag = new Acceuil_gerant();
+               Util.id_agent_connecte = agent2.getId();
               ag.setVisible(true);
               this.setVisible(false);
           }
@@ -94,8 +102,8 @@ public class Authentification extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        label_login = new javax.swing.JLabel();
+        label_pass = new javax.swing.JLabel();
         login = new javax.swing.JTextField();
         bt_se_connecter = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
@@ -111,11 +119,11 @@ public class Authentification extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
-        jLabel1.setText("Login :");
+        label_login.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
+        label_login.setText("Login :");
 
-        jLabel2.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
-        jLabel2.setText("Pass :");
+        label_pass.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
+        label_pass.setText("Pass :");
 
         bt_se_connecter.setText("Se connecter");
         bt_se_connecter.addActionListener(new java.awt.event.ActionListener() {
@@ -163,11 +171,11 @@ public class Authentification extends javax.swing.JFrame {
                 .addGap(49, 49, 49)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(label_pass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(41, 41, 41)
                         .addComponent(pass, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(label_login, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(35, 35, 35)
                         .addComponent(login, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 174, Short.MAX_VALUE)
@@ -192,13 +200,13 @@ public class Authentification extends javax.swing.JFrame {
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(label_login, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(13, 13, 13)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
+                    .addComponent(label_pass))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -215,6 +223,7 @@ public class Authentification extends javax.swing.JFrame {
 
     private void bt_se_connecterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_se_connecterActionPerformed
        seConnecter();
+        System.out.println("bouton se connecter presser");
     }//GEN-LAST:event_bt_se_connecterActionPerformed
 
     private void bt_inscrireActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_inscrireActionPerformed
@@ -275,11 +284,11 @@ public class Authentification extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bt_inscrire;
     private javax.swing.JButton bt_se_connecter;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel label_login;
+    private javax.swing.JLabel label_pass;
     private javax.swing.JTextField login;
     private javax.swing.JPasswordField pass;
     // End of variables declaration//GEN-END:variables
