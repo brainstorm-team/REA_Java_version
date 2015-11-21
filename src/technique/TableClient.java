7c8d010043001ef8a8d05d2622aa2871ba51e1e9
Ben jemaa Chokri <chokri.benjemaa@esprit.tn>
2015-11-21 18:18:25 +0100

    Update TableClient.java

--- src/technique/TableClient.java
+++ src/technique/TableClient.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package technique;
 
 import DAO.classes.ClientDAO;
@@ -12,10 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
-/**
- *
- * @author anwer
- */
 public class TableClient extends AbstractTableModel {
 
     List<Client> listclient = new ArrayList<Client>();
